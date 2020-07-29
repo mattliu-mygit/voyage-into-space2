92343201f2fb9c7f650470ee7a06ea8bc2cdfcb3
MattLiu-mygit <mliu78@jh.edu>
2020-07-29 13:28:10 -0400

    Slight adjustment for mobile clients

--- client/src/components/StartPage.js
+++ client/src/components/StartPage.js
@@ -5,10 +5,10 @@ function StartPage(props) {
   const [fontSize, setFontSize] = useState(0);
   const [blastOff, setBlastOff] = useState(false);
   useEffect(() => {
-    if (fontSize < 11) {
+    if (fontSize < 10) {
       setTimeout(() => {
-        setFontSize(fontSize + 0.05);
-      }, 20);
+        setFontSize(fontSize + 0.005);
+      }, 1);
     } else {
       setBlastOff(true);
     }
@@ -16,18 +16,21 @@ function StartPage(props) {
   return (
     <>
       <div className="placeholder"> |</div>
-      <h1
+      <div
         style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
           color: 'white',
-          textAlign: 'center',
+          //textAlign: 'center',
           marginTop: '12.5%',
-          fontSize: `${fontSize}rem`,
+          fontSize: `${fontSize}vw`,
         }}
       >
         <b>
           <i>Voyage into Space</i>
         </b>
-      </h1>
+      </div>
       {blastOff ? (
         <Link
           to="/blast-off"
@@ -35,7 +38,7 @@ function StartPage(props) {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            fontSize: '3rem',
+            fontSize: '2.5vw',
             marginTop: '10rem',
             color: '#F1F4FFFF',
           }}
