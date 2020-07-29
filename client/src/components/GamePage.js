d95fd4307f9df8053eb376efb82efc9cef18f0d5
MattLiu-mygit <mliu78@jh.edu>
2020-07-29 17:57:24 -0400

    GamePage inner-window implemented

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import launchsite from '../launchsite.jpg';
 
+const styles = {
+  gameStyle: {
+    width: '90%',
+    borderRadius: '12px',
+    border: 'solid',
+    borderColor: 'silver',
+    boxShadow: 'black',
+    borderWidth: '1rem',
+    margin: '5%',
+    marginTop: '2%',
+    marginBottom: '0rem',
+    backgroundImage: `url(${launchsite})`,
+    height: '50vw',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    padding: '1rem',
+    paddingLeft: '2rem',
+    borderBottomColor: 'grey',
+    borderTopColor: 'grey',
+    color: '#39FF14',
+    fontSize: '1.75vw',
+    fontFamily: 'Lucida Console, Courier, monospace',
+  },
+};
+
 const GamePage = () => {
+  const [word1Num, setWord1Num] = useState(1);
+  const [word2Num, setWord2Num] = useState(1);
+  const [site, setSite] = useState('');
+  const [date, setDate] = useState('');
+  const word1 = 'Voyage Inc. Desert Launch Site';
+  const word2 = new Date().toString();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (word1Num <= word1.length) {
+        setWord1Num(word1Num + 1);
+        setSite(site + word1[word1Num - 1]);
+      } else if (word2Num <= word2.length) {
+        setWord2Num(word2Num + 1);
+        setDate(date + word2[word2Num - 1]);
+      }
+    }, 100);
+  }, [site.length, date.length]);
+
   return (
     <>
       <div className="placeholder"> |</div>
-      <img
-        style={{
-          width: '90%',
-          borderRadius: '12px',
-          border: 'solid',
-          borderColor: 'silver',
-          boxShadow: 'black',
-          borderWidth: '1rem',
-          margin: '5%',
-          marginTop: '2rem',
-          marginBottom: '0rem',
-        }}
-        src={launchsite}
-        alt="uh oh, launch site pic not found"
-      />
+      <div style={styles.gameStyle}>
+        <p>{site}</p> <p>{date}</p>
+      </div>
     </>
   );
 };
